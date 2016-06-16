9ba47c9232392974a86fcffbf7cc66d4ae469c34
frasator <frasator@gmail.com>
2016-06-16 19:33:45 +0200

    Added genome viewer.

--- conf/config.js
+++ conf/config.js
@@ -3,3 +3,4 @@
  * Changes to this file may cause the application does not work as it should
  **/
 BAM_SERVER_HOST = 'http://localhost:1111';
+CELLBASE_HOST = 'http://bioinfodev.hpc.cam.ac.uk/cellbase';
