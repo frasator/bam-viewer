0015c608fcdad1aff6ab9c08ffae4f40509fd528
frasator <frasator@gmail.com>
2016-06-21 14:26:42 +0200

    Minor fixes

--- conf/config.js
+++ conf/config.js
@@ -2,5 +2,6 @@
  * This is a configuration file.
  * Changes to this file may cause the application does not work as it should
  **/
-BAM_SERVER_HOST = 'http://mem16:1111';
+BAM_SERVER_HOST = 'http://ws.babelomics.org/bv01';
+// BAM_SERVER_HOST = 'http://localhost:8501';
 CELLBASE_HOST = 'http://bioinfo.hpc.cam.ac.uk/cellbase';
